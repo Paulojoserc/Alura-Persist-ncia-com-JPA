1af1def4024bffa88c2c9a628f91d0b6478137a4
Paulo Costa <pjrc83@gmail.com>
2023-04-20 17:31:37 -0300

    Relacionamentos many-to-many

--- loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -2,12 +2,15 @@ package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -24,6 +27,9 @@ public class Pedido {
 	
 	@ManyToOne
 	private Cliente cliente;
+	
+	@OneToMany
+	private List<ItemPedido> itens;
 	
 	public Pedido() {
 	}
