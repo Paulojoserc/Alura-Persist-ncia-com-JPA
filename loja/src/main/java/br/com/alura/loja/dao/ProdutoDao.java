e1691319a09f8f04cf62de4b09d3d984e249c1ff
Paulo Costa <pjrc83@gmail.com>
2023-04-20 16:09:55 -0300

    Consultando entidades

--- loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
@@ -13,5 +15,22 @@ public class ProdutoDao {
 	
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
+	}
+	
+	public void atualizar(Produto produto) {
+	    this.em.merge(produto);
+	}
+	
+	public void remover (Produto produto) {
+		produto =  em.merge(produto);
+		this.em.remove(produto);
+	}
+	public Produto buscarPorId(Long id) {
+		//em.find serve para uma unica intidade que receber dois parametros um para dizer a intidade e outro o id
+		return em.find(Produto.class, id);
+	}
+	public List<Produto> buscarTodos(){
+		String jpql = "SELECT P FROM Produto p";
+		return em.createQuery(jpql, Produto.class).getResultList();
 	}
 }
--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,41 +1,59 @@
 package br.com.alura.loja.testes;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 	public static void main(String[] args) {
+		cadastrarProduto();
+				
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		
+		Produto p = produtoDao.buscarPorId(1l);
+		/*System.out.println(p.getPreco());
+		*/
+		List<Produto> todos = produtoDao.buscarTodos();
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
 
-		// Produto celular = new Produto("Xiaomi Redmi","Maito legal",new
-		// BigDecimal("800"),celulares);
+		Produto celular = new Produto("Xiaomi Redmi", "Maito legal", new BigDecimal("800"), celulares);
 
 		EntityManager em = JPAUtil.getEntityManager();
-		// ProdutoDao produtoDao = new ProdutoDao(em);
-		// CategoriaDao categoriaDao = new CategoriaDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 
 		// Pede para jpa pegar a transação
 		em.getTransaction().begin();
 
-		// categoriaDao.cadastrar(celulares);
+		categoriaDao.cadastrar(celulares);
 		// O que vc quer fazer de operação
-		// produtoDao.cadastrar(celular);
+		produtoDao.cadastrar(celular);
 		// Faz o commit
 		em.persist(celulares);
 		celulares.setNome("XPTO");
-		//Dispara um update no banco do em.persist
+		// Dispara um update no banco do em.persist
 		em.flush();
 		em.clear();
-		//Margem faz um select no banco
+		// Margem faz um select no banco
 		celulares = em.merge(celulares);
 		celulares.setNome("1234");
 		em.flush();
-	//	em.clear();
-		//Deletar no banco a entidade
+		// em.clear();
+		// Deletar no banco a entidade
 		em.remove(celulares);
 		em.flush();
-
 	}
 }
